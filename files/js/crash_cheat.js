b1afb80dae9f005f371d3b5070f00e0629e405c6
Fuxx-1 <583742849@qq.com>
2021-02-24 23:48:34 +0800

    Site updated: 2021-02-24 23:48:30

--- files/js/crash_cheat.js
+++ files/js/crash_cheat.js
@@ -1,15 +1,16 @@
-<!--浏览器搞笑标题-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
          document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~';
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
-         }, 100);
+         }, 2000);
      }
  });
