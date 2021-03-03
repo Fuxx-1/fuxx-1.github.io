3fa226132f21221890ff59c4964d960a99bf6a70
Fuxx-1 <583742849@qq.com>
2021-03-04 07:25:38 +0800

    Site updated: 2021-03-04 07:25:35

--- files/js/cue.js
+++ files/js/cue.js
@@ -1,6 +1,6 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
   // 执行相应代码或直接跳转到手机页面
-  alert("网页端访问体验更佳！");
+  alert("Fuxxの博客提醒您：\n电脑访问体验更佳！");
 } else {
   // 执行桌面端代码
 }
